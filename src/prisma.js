ca077d34e3168d7072d4dbffbe0ba18aa680196e
Kennedy Otieno <nyagaotieno@gmail.com>
2026-02-25 00:59:53 -0800

    Export prisma as default and named

--- src/prisma.js
+++ src/prisma.js
@@ -1,5 +1,10 @@
 // src/prisma.js
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = globalThis.__prisma || new PrismaClient();
+
+// avoid hot-reload creating many clients in dev
+if (process.env.NODE_ENV !== "production") globalThis.__prisma = prisma;
+
+export { prisma };
 export default prisma;
